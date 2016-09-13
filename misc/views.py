37e6d5c5f17e43ed21df580ec30f7a68ac17e118
Kevin Cook <kevinac4@gmail.com>
2016-09-12 19:13:13 -0700

    removing logging

--- misc/views.py
+++ misc/views.py
@@ -1,6 +1,6 @@
 import jwt
 import requests
-import error
+import logging
 
 from django.shortcuts import render
 from django.core.files import File
@@ -19,14 +19,10 @@ def jwt_response_payload_handler(token, user=None, request=None):
 def save_image_from_url(model, url):
     logger = logging.getLogger(__name__)
 
-    try:
-        r = requests.get(url)
-        from urllib import parse
-        filename = parse.urlparse(url).path.split('/')[-1]
-        img_temp = NamedTemporaryFile(delete=True)
-        img_temp.write(r.content)
-        img_temp.flush()
-        model.image.save("image.jpg", File(img_temp), save=True)
-    except Exception as e:
-                logger.error('Something went wrong!')
-                logger.error(e)
+    r = requests.get(url)
+    from urllib import parse
+    filename = parse.urlparse(url).path.split('/')[-1]
+    img_temp = NamedTemporaryFile(delete=True)
+    img_temp.write(r.content)
+    img_temp.flush()
+    model.image.save("image.jpg", File(img_temp), save=True)
