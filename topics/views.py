1a9a9bb406ccdf638a57965ec89ccbf79f5088b6
Kevin Cook <kevinac4@gmail.com>
2016-11-30 15:23:24 -0800

    opengraph dimensions

--- topics/views.py
+++ topics/views.py
@@ -6,6 +6,8 @@ from .models import *
 # Create your views here.
 def topic_details(request, pk):
 	a = Topic.objects.get(pk=pk)
+	width = a.image.width
+	height = a.image.height
 	return HttpResponse(
 		"""
 		<!doctype html>
@@ -15,13 +17,15 @@ def topic_details(request, pk):
 		      <meta charset="utf-8">
 		      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		      <title>{title}</title>
-		      <meta content="{title}" property="og:title" />
-		      <meta content="{image}" property="og:image" />
+		      <meta property="og:title" content="{title}"/>
+		      <meta property="og:image:width" content="{width}" />
+		      <meta property="og:image:height" content="{height}" />
+		      <meta property="og:image" content="{image}" />
 		  </head>
 		  <body>
 		  	<h1>{title}</h1>
 		  	<img src="{image}" />
 		  </body>
 		</html>
-		""".format(title="Get Involved | " + a.title, image=a.image.url)
+		""".format(title="Get Involved | " + a.title, image=a.image.url, height=height, width=a.image.width)
 	)
