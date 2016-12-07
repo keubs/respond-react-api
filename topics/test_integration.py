404264efb9792cb3176cf84bfeda60f7b8067b4f
Jonathan D. Baker <jbaker@comverge.com>
2016-12-07 11:10:25 -0700

    topic_detail test

--- topics/test_integration.py
+++ topics/test_integration.py
@@ -7,7 +7,21 @@ from topics.factories import TopicFactory
 from untitled.testing import BaseAPITestCase
 
 
-class TopicListHelpersTestCase(BaseAPITestCase):
+class TopicApiDetailTestCase(BaseAPITestCase):
+
+    def test_get_ok(self):
+        image = self.create_test_image()
+        user = CustomUserFactory.create()
+        topic = TopicFactory.create(created_by=user, image=image)
+
+        response = self.client.get(
+            reverse("topic_detail", kwargs={"pk": topic.id}))
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(data["created_by"], topic.created_by.id)
+
+
+class TopicApiListTestCase(BaseAPITestCase):
 
     def test_get_ok(self):
         image = self.create_test_image()
