feae700b65813e0c36ac599e66843b500227cb66
Kevin Cook <kevinac4@gmail.com>
2017-01-12 12:14:29 -0800

    another test

--- topics/api.py
+++ topics/api.py
@@ -390,6 +390,7 @@ class ActionListByTopic(APIView):
         #     # If page is not an integer, deliver first page.
         #     actions = paginator.page(1)
         # except EmptyPage:
+        #     # If page is out of range (e.g. 9999), deliver last page of results.
         #     actions = paginator.page(paginator.num_pages)
 
         payload = []
