b267676174930b1fce129157215a794892904e4d
Jonathan D. Baker <jbaker@comverge.com>
2017-01-28 17:20:01 -0700

    create dummy data

--- addressapi/factories.py
+++ addressapi/factories.py
@@ -10,3 +10,32 @@ class AddressFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = "address.Address"
+
+
+class CountryFactory(factory.DjangoModelFactory):
+
+    code = factory.Sequence(lambda n: "{0}".format(n))
+    name = factory.Sequence(lambda n: "name-{0}".format(n))
+
+    class Meta:
+        model = "address.Country"
+
+
+class StateFactory(factory.DjangoModelFactory):
+
+    code = factory.Sequence(lambda n: "{0}".format(n))
+    country = factory.SubFactory(CountryFactory)
+    name = factory.Sequence(lambda n: "name-{0}".format(n))
+
+    class Meta:
+        model = "address.State"
+
+
+class LocalityFactory(factory.DjangoModelFactory):
+
+    postal_code = factory.Sequence(lambda n: "postal_code-{0}".format(n))
+    name = factory.Sequence(lambda n: "name-{0}".format(n))
+    state = factory.SubFactory(StateFactory)
+
+    class Meta:
+        model = "address.Locality"
--- untitled/settings/base.py
+++ untitled/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'addressapi',
     'linkfactory',
     'imagekit',
+    "utils",
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
